451849fc2796a67a3151dd21ef0878a8bfba6650
zhy <zhyjqq@gmail.com>
2017-09-06 10:48:27 +0800

    Initial Commit

---
+++ OpenGL-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGL-Demo
+//
+//  Created by user on 2017/9/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGL-Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGL-Demo
+//
+//  Created by user on 2017/9/6.
+//  Copyright © 2017年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
